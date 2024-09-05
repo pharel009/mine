b6ebd70b4001949563a301d1b0cd57d51b1c4401
Pharel <pharelborfo@gmail.com>
2024-09-05 10:24:25 +0100

    i made a change

--- src/user/user.model.js
+++ src/user/user.model.js
@@ -5,6 +5,7 @@ export const createUserTable = async () => {
     CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
         password VARCHAR(100) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )`;
